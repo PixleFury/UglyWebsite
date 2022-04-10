63ae37153c8818874a9eeae333d39be90e31cf53
PixleFury <pixlefury@outlook.com>
2022-04-10 20:57:20 +0000

    Completed user account system and added login page

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require("express");
-const path = require("path");
+const session = require("express-session");
 
 // Create server and configure settings
 const app = express();
 app.use(express.static("public"));
+// TODO: Set secure to true.  Needs to be false to run on local machine
+// Cookie expires in 60 minutes (in milliseconds)
+app.use(session({cookie: {maxAge: 60 * 60 * 1000, secure: false}, secret: "54312"}));
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-	res.render("home", {user: null});
+	res.render("home", {user: req.session.user});
 });
 
 app.get("/play", (req, res) => {
-	res.render("play", {user: null});
+	res.render("play", {user: req.session.user});
 });
 
 app.get("/comments", (req, res) => {
-	res.render("comments", {user: null});
+	res.render("comments", {user: req.session.user});
 });
 
 app.use(require("./routers/account"));
--- routers/account.js
+++ routers/account.js
@@ -1,22 +1,86 @@
 const express = require("express");
+const { Collection } = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
 const router = express.Router();
 
+const DATABASE_URI = `mongodb+srv://ugly-website:${process.env.DB_PASSWORD}@cluster0.yuxd3.mongodb.net/test?retryWrites=true&w=majority`;
+let users;
+let comments;
+
+MongoClient.connect(DATABASE_URI, (err, client) => {
+	if (err) throw err;
+	const db = client.db("uglywebsite");
+	users = db.collection("users");
+});
+
+router.get("/profile/:username", (req, res) => {
+	let username = req.params.username;
+
+	users.findOne({username: username}, (err, doc) => {
+		if (err) throw err;
+
+		if (!doc) {
+			res.sendStatus(404);
+			return;
+		}
+
+		let can_edit = false;
+
+		if (req.session.user) {
+			can_edit = req.session.user.is_admin || req.session.user.username == doc.username;
+		}
+
+		res.render("profile", {user: req.session.user, profile: doc, can_edit: can_edit});
+	});
+});
+
 router.get("/login", (req, res) => {
-	res.render("login", {user: null});
+	res.render("login", {user: req.session.user});
 });
 
 router.post("/login", (req, res) => {
-	console.log(req.body);
-	res.redirect("/");
+	let username = req.body["username"];
+	let password = req.body["password"];
+
+	users.findOne({username: username, password: password}, (err, doc) => {
+		if (err) throw err;
+
+		if (!doc) {
+			res.redirect("/login?error=incorrect-details");
+			return;
+		}
+
+		req.session.user = doc;
+		res.redirect("/");
+	});
 });
 
 router.post("/register", (req, res) => {
-	console.log(req.body);
-	res.redirect("/");
+	let username = req.body["username"];
+	let password = req.body["password"];
+	let password_copy = req.body["password-copy"];
+
+	users.findOne({username: username}, (err, doc) => {
+		if (err) throw err;
+
+		if (doc) {
+			res.redirect("/login?error=username-taken");
+			return;
+		}
+
+		if (password != password_copy) {
+			res.redirect("/login?error=pw-no-match");
+			return;
+		}
+
+		users.insertOne({username: username, password: password, is_admin: false});
+		req.session.user = {username: username, is_admin: false};
+		res.redirect("/");
+	});
 });
 
-router.post("/logout", (req, res) => {
-	console.log(req.body);
+router.get("/logout", (req, res) => {
+	req.session.user = null;
 	res.redirect("/");
 });
 
